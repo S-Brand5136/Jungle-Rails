c2c7f9208c66542c8474679d29e7a72f8cd41a08
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-24 17:55:28 +0000

    add HTML and styling for admin/categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,7 +2,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'] ,password: ENV['PASSWORD']
 
   def index
-    @categories = Category.order(id: :desc).all
+    @categorys = Category.order(id: :desc).all
   end
 
   def new
