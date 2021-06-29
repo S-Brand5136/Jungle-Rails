0440c79cbe1df2ef23d5ce500d2bf0178fe89275
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-29 19:00:01 +0000

    add before setup to gen products

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -46,7 +46,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
   # RSpec Rails can automatically mix in different behaviours to your tests
