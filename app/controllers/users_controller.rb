2acd7e5f0bd3b59fecc86809b06eaf41cdd65b48
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-29 00:00:28 +0000

    add user fields and edge case tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
@@ -19,7 +20,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(
       :first_name, 
       :last_name, 
-      :email, 
+      :email.downcase,
       :password, 
       :password_confirmation
     )
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, length: { minimum: 5 }
+
+  before_save { self.email.downcase! }
+
+  def self.authenticate_with_credientials(email, password)
+    user = User.find_by_email email.strip.downcase
+    if (user && user.authenticate(password))
+      return user
+    else
+      nil
+    end
+  end
+
 end
